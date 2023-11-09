354897f349402b6e6312b5164cd471bd4fdfc984
BLealan <143086275+BLealan@users.noreply.github.com>
2023-11-09 16:26:32 +0000

    linked search button to recipe API fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 var apikey = "1";
+
 function getApi() {
-    // replace `octocat` with anyone else's GitHub username
-    var requestUrl = 'https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
+// replace `octocat` with anyone else's GitHub username
+	
+var userSearch = $("#search-input").val();
+	console.log(userSearch);
+    var requestUrl = `https://themealdb.com/api/json/v1/1/search.php?s=${userSearch}`;
   
     fetch(requestUrl)
       .then(function (response) {
@@ -9,8 +13,12 @@ function getApi() {
       })
       .then(function (data) {
         console.log(data);
-      });
-  }
-  getApi();
+      })}
+	  ;
 
-  
+$("#search-button").on("click", function(){
+	event.preventDefault()
+
+	getApi();
+
+});
