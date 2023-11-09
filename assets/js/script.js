04b4300b7424f58d52634253415c87536b2f15ef
lalitakapadia <lalita.kapadia@gmail.com>
2023-11-09 17:43:22 +0000

    worked on javascript and tailwind css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,82 @@
-var apikey = "1";
-function getApi() {
-    // replace `octocat` with anyone else's GitHub username
-    var requestUrl = 'https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
+const apikey = "1";
+function getMealdbByName(mealName) {
+  //search mealdb api by ingredient
+  const requesMealdbUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + mealName;
+
+  fetch(requesMealdbUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      //calling mealdata function
+      displayMealData(data);
+    });
+}
+
+// add event listener for search button
+$('#searchButton').on('click',searchMealEvent);
+function searchMealEvent(event){
+  event.preventDefault();
+  //get value from he text box
+  var mealName = $("#search-input").val().trim();
+  console.log(mealName);
+  if(mealName != ""){
+    getMealdbByName(mealName);
+  }  
+}
+ //display  data to the users
+function displayMealData(data){
+  $('#recipe-deck').empty();
+  for(i = 0; i < data.meals.length; i++){
+     //create dynamic elements with tailwind css style
+    var displayCard = $('<div class="max-w-sm w-full lg:max-w-full lg:flex mb-2 ml-5 ">');
+    
+    // image display with tailwind css 
+    var imgDiv = $('<div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">');
+   
+    imgDiv.attr('style', 'background-image: url(' + data.meals[i].strMealThumb + ')');
+    displayCard.append(imgDiv);
+
+    // recipe display div with tailwind css style
+    // declare variables with html and tailwind css
+    var recipeDiv = $('<div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">');
+    
+    var mealP = $('<p>');
+    var categoryP = $('<p>');
+    var instructionsPValue = $('<p>');
+    var instructionsPLable = $('<p>');
+    var tagsH4 = $('<h4>');
+   
+    //recipe name
+    mealP.text((data.meals[i].strMeal));
+    mealP.addClass("text-gray-900 font-bold text-xl mb-2");
+    //recipe category like vegetarian or non vegetarian , recipe area origion from
+    categoryP.text((data.meals[i].strCategory) + ', ' + (data.meals[i].strArea));
+    categoryP.addClass("text-gray-700 text-base");
+   //instuction for recipe and bold text 
+    instructionsPLable.text('Instuctions: ');
+    instructionsPLable.addClass('text-gray-700 text-base font-bold');
+    //in grey text full instuction
+    instructionsPValue.text(data.meals[i].strInstructions);
+    instructionsPValue.addClass('text-gray-700 text-base');
+    //
+    tagsH4.text('Tags: ' + (data.meals[i].strTags));
+    tagsH4.addClass('text-gray-700 text-base');
+
+
+
+    recipeDiv.append(mealP);
+    recipeDiv.append(categoryP);
+    recipeDiv.append(instructionsPLable); 
+    recipeDiv.append(instructionsPValue); 
+    recipeDiv.append(tagsH4);
+
+    displayCard.append(recipeDiv);
+
+    $('#recipe-deck').append(displayCard);
   }
-  getApi();
+}
+ 
 
   
