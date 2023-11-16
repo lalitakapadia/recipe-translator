03b948efffaa8401c37df8a26e32a096270f5116
lalitakapadia <lalita.kapadia@gmail.com>
2023-11-16 14:09:34 +0000

    modify some minor changes and added comments

--- assets/js/languages.js
+++ assets/js/languages.js
@@ -1,6 +1,7 @@
-//Function to generate language options
+//// FLOW-3: fetch supported Languages - Function to generate language options
 //API fetch request to get object with language codes supported by lectoAPI
 function generateLanguageOptionsLecto(){
+  // FLOW-3-1 setup api settings such as url, api key etc
 	const settings = {
     async: true,
     crossDomain: true,
@@ -12,48 +13,47 @@ function generateLanguageOptionsLecto(){
     }
   };
   
-  //Use returned data to create user inputs(drop down menu, translate buttons)
   $.ajax(settings).done(function (response) {
 	var languageArray = response.languages;
-    //var userInputDiv = $(`<div class="user-input gap-4">`);
+   // FLOW-3-2 create drop down list with default option
     var dropDown = $('#languages-lecto');
     var defaultDropDown = $(`<option value="" disabled selected>Select a language</option>`);
     dropDown.append(defaultDropDown);
-    //var translateIngredientButton = $(`<button type="submit" id="translate-ingredient-btn" class="mx-1 text-black bg-yellow-500 hover:bg-yellow-600  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Translate Ingredients</button>`);
-    //var translateMethodButton = $(`<button type="submit" id="translate-method-btn" class="mx-1 text-black bg-yellow-500 hover:bg-yellow-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Translate Instructions</button>`);
-	  //for loop iterates data object to create each language option with code to be used in translation function
+	  // FLOW-3-3 for loop iterates data object to create each language option with code to be used in translation function
     for (var i = 0; i < languageArray.length; i++){
       var languageName = languageArray[i].display_name;
       var languageCode = languageArray[i].language_code;
       var languageOption = $(`<option value="${languageCode}">${languageName}</option>`);
       dropDown.append(languageOption);
     }
-    //userInputDiv.append(dropDown, translateIngredientButton, translateMethodButton);
-    //$(".recipe-deck-content").append(userInputDiv);
-    dropDown.on("change",doTranslation);
+    //FLOW-3-4 add event listener for change
+    dropDown.on("change",triggerTranslation);
   })
 
 };
 
-
-function doTranslation(event){
-    //alert($(this).val());
-    var mealDb = JSON.parse(localStorage.getItem("mealDb"));
-    if(mealDb != null){
-        var languageCode = $(this).val();
-
-        for(i=0; i<mealDb.meals.length; i++){
-          
-          var instruction = $(`#instruction-${i}`);
-          var instructionText = instruction.html();
-          //translate(languageCode,oldText,instruction);
-          setTimeout(function(){translate(languageCode, instructionText, instruction)}, 2000);
-          setTimeout(translate, 5000, languageCode, instructionText, instruction);
-          //setTimeout(function(){translate(languageCode, $(`#instruction-${i}`).html(), $(`#instruction-${i}`))}, 2000);
-          var r = $(`#recipe-${i}`);
-          var recipeText = r.html();
-          //translate(languageCode,oldText,r);
-          setTimeout(function(){translate(languageCode, recipeText, r)}, 2000);
-        }
+ //FLOW-3-4 this triggers translation for each recipe
+function triggerTranslation(event){
+  //FLOW-3-4-1 get the mealdb data from local storage to loop throgh number of recipes
+  var mealDb = JSON.parse(localStorage.getItem("mealDb"));
+  //FLOW-3-4-2 if data is not null 
+  if(mealDb != null){
+    //FLOW-3-4-3 get selected languagecode from dropdown
+    var languageCode = $(this).val();
+    //FLOW-3-4-4 loop through all recipes
+    for(i = 0; i < mealDb.meals.length; i++){
+      //FLOW-3-5 get the instruction for given recipe
+      var instruction = $(`#instruction-${i}`);
+      //FLOW-3-6 get the html from the instruction div for given recipe
+      var instructionText = instruction.html();
+      //FLOW-3-4-7 call the translate function for instruction with a time dealy due to api call rate limit
+      setTimeout(function(){translate(languageCode, instructionText, instruction)}, 1000 * (i + 1));
+       //FLOW-3-4-8 get the recipe div for the given recipe
+      var r = $(`#recipe-${i}`);
+        //FLOW-3-4-9 get the recipe div html that contains title ingredients and measurement
+      var recipeText = r.html();
+       //FLOW-3-4-10 call the translate function for ingredient with a time dealy due to api call rate limit
+      setTimeout(function(){translate(languageCode, recipeText, r)}, 1000 * (i + 3));
     }
+  }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
+//mealdb API key
 const apikey = "1";
 // declared var for autogenerate meal name
 var availableTags = [];
 
+// FLOW-1: Page load, initialization, configuration etc.
+// FLOW-2: Search button click
+// FLOW-3: fetch supported Languages
+// FLOW-4: trnslation
+// FLOW-5: Auto Complete
+
+// Function gets called in FLOW-2:
 function getMealdbByName(mealName) {
-  //search mealdb api by ingredient
+
+ // FLOW-2-4: fetch mealdb api url
   const requestMealdbUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + mealName;
 
   fetch(requestMealdbUrl)
@@ -12,7 +21,7 @@ function getMealdbByName(mealName) {
     })
     .then(function (data) {
       console.log(data);
-      //calling mealdata function
+      //FLOW-2-5: received meal db data, now call display fuction
       displayMealData(data);
 
       localStorage.setItem("mealDb",JSON.stringify(data));
@@ -20,79 +29,95 @@ function getMealdbByName(mealName) {
     });
 }
 
-//funtion for autogenrate, mealname stores in local storage 
+//flow-1 page load - funtion for autogenrate, mealname stores in local storage 
 $(function() {
+  //Flow-1-1 get the data from local storage
   availableTags = JSON.parse(localStorage.getItem("recipe"));
-  //when meal name is null that one not stored in locarstorage
+   //Flow-1-2 check if the data exist
   if(availableTags == null){
     availableTags = [];
   }
-  //this stored mealname will autogenerate when  the search button
+  // //Flow-1-3 - this stored mealname will autogenerate when  the search button
   $('#search-input').autocomplete({
     source: availableTags
   });
   } 
 );
-// add event listener for search button
+
+
+//FLOW-2: add event listener for search button
 $('#searchButton').on('click',searchMealEvent);
 function searchMealEvent(event){
   event.preventDefault();
-  //get value from the text box
+  //FLOW-2-1:get value from the text box
   var mealName = $("#search-input").val().trim();
   console.log(mealName);
+
+  // FLOW-2-2: check that text box has value
   if(mealName != ""){
+    // FLOW-2-3: get meal db data
     getMealdbByName(mealName);
 
+    // FLOW-2-4 set defult language to english
     $('#languages-lecto').val('en');
 
-   // function for (autocomplete) that stores meal name in localstorage 
-  if(availableTags.indexOf(mealName) == -1){
+    // FLOW-2-5 for (autocomplete) that stores meal name in localstorage 
+    if(availableTags.indexOf(mealName) == -1){
+      // FLOW-2-6 mealname does not exist in local storage, add it to array
       availableTags.push(mealName);
+      // FLOW-2-7 store the latest array to local storage
       localStorage.setItem('recipe', JSON.stringify(availableTags));
     }
     
-    //user-instruction "how to use" will hide from the page when clicked on search button
+    //// FLOW-2-8 user-instruction "how to use" will hide from the page when clicked on search button
     $('.user-instruction').hide();
   }  
 }
 
- //display  data to the users
+ // FLOW-2-5  data comes in loops 
 function displayMealData(data){
   $('#recipe-deck').empty();
+  // FLOW-2-5-1 loop through all the meals
   for(i = 0; i < data.meals.length; i++){
-    //create dynamic elements with tailwind css style
-    if (data.meals[i].strInstructions.length>1000){
-      continue
-    }
+   
+    // to omit any recipe above 1000 character due to api limit
+    //if (data.meals[i].strInstructions.length>1000){
+    //  continue
+    // }
+
+    //FLOW-2-5-2 create display card dynamic html with tailwind css style
     var displayCard = $('<div class=" recipe-deck-content lg:flex mb-2 ml-5 mr-5">');
     
-    // display images,title,ingredients,recipe instruction
+    //FLOW-2-5-3 display recipe image
     addImage(data, i, displayCard);
+    //FLOW-2-5-4 display title, ingredients, measurement
     addRecipe(data, i, displayCard);
+    //FLOW-2-5-5 display recipe instruction
     addRecipeInstruction(data, i, displayCard);
     
-    //add display card to recipe div
+    // FLOW-2-5-6 add display card to recipe deck div
     $('#recipe-deck').append(displayCard);
 
-    //save each recipe (key = name, value = data object) to local storage
+    // FLOW-2-5-7 save each recipe (key = name, value = data object) to local storage, if needed for future use
     var savedRecipe = data.meals[i].strMeal;
     var savedObject = JSON.stringify(data.meals[i]);
     localStorage.setItem(savedRecipe, savedObject)
   }
 }
-
+//FLOW-2-5-3 to display recipe image
 function addImage(data, index, displayCard){
-    // image displays with tailwind css style
+    // FLOW-2-5-3-1 image displays with tailwind css style
     var imgDiv = $('<div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden lg:w-1/4">');
-   
+   // FLOW-2-5-3-2 set url for recipe image
     imgDiv.attr('style', 'background-image: url(' + data.meals[index].strMealThumb + ')');
+    // FLOW-2-5-3-2 add image div to display card
     displayCard.append(imgDiv);
 }
 
+  //FLOW-2-5-4 display title, ingredients, measurement for the given recipe
 function addRecipe(data, i, displayCard){
-  // recipe display div with tailwind css style
-  //  to display title, ingredients, category and area of the meal
-   
+
+  // FLOW-2-5-4-1 create div, paragraphs and list html elements with tailwind css style
   var recipeDiv = $(`<div id="recipe-${i}"  class="info border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 lg:w-1/4 flex flex-col justify-top leading-normal">`);
   
   var mealP = $('<p>');
@@ -104,20 +129,20 @@ function addRecipe(data, i, displayCard){
   var ingredientUL = $('<ul>');
   ingredientUL.addClass('ingredient-list text-gray-700 text-base list-disc ml-5');
 
-  //recipe name and category
+  // FLOW-2-5-4-2 display recipe title
   mealP.text((data.meals[i].strMeal));
   mealP.addClass("text-gray-900 font-bold text-xl mb-2");
-  //recipe category like vegetarian or non vegetarian , recipe area origin from
+
+  //FLOW-2-5-4-3 recipe category like vegetarian or non vegetarian , recipe area origin from
   categoryP.text((data.meals[i].strCategory) + ', ' + (data.meals[i].strArea));
   categoryP.addClass("text-gray-700 text-base");    
 
-  //loop for ingredients x = ingredients and measurement string
-  for(x=1; x<21; x++){
- 
+  // FLOW-2-5-4-4 loop for ingredients x = ingredients and measurement string
+  for(x = 1; x < 21; x++){
     var ingredientElement = 'strIngredient' + x;
     var ingredient = data.meals[i][ingredientElement];
 
-    // loop for measurements
+    // fetch measurement from mealdb and add to displaycard
     var measureElement = 'strMeasure' + x;
     var measure = data.meals[i][measureElement];
     
@@ -132,58 +157,58 @@ function addRecipe(data, i, displayCard){
   displayCard.append(recipeDiv);
 }
 
-
+// FLOW-2-5-5 function for instruction
 function addRecipeInstruction(data, i, displayCard){
-  // instruction for recipe display div with tailwind css style
+  // FLOW-2-5-5-1 create div, paragraphs html elements with tailwind css style
   var instructionDiv = $(`<div id="instruction-${i}"  class="instructions border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 lg:w-1/2 flex flex-col text-justify">`);
   var instructionsPValue = $(`<p>`);
   var instructionsPLable = $('<p>');
-  //instruction text in bold text 
+  // FLOW-2-5-5-2 instruction text in bold text 
   instructionsPLable.text('Instructions: ');
   instructionsPLable.addClass('instructions-title text-gray-700 text-base font-bold');
-  //in grey text full instruction
+  // FLOW-2-5-5-3 in grey text full instruction
   instructionsPValue.text(data.meals[i].strInstructions);
   instructionsPValue.addClass('content text-gray-700 text-base');
   instructionDiv.append(instructionsPLable); 
   instructionDiv.append(instructionsPValue); 
   displayCard.append(instructionDiv);
-  //generateLanguageOptions();
+  //generateLanguageOptions(); // for future use
 }
 
 //Function to generate language options
 //API fetch request to get object with language codes supported by lectoAPI
-function generateLanguageOptions(){
-	const settings = {
-    async: true,
-    crossDomain: true,
-    url: 'https://lecto-translation.p.rapidapi.com/v1/translate/languages',
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'bc97a45c89msh3eda3b2b978fb1fp1f44c1jsn30893d3a6b96',
-      'X-RapidAPI-Host': 'lecto-translation.p.rapidapi.com'
-    }
-  };
+// function generateLanguageOptions(){
+// 	const settings = {
+//     async: true,
+//     crossDomain: true,
+//     url: 'https://lecto-translation.p.rapidapi.com/v1/translate/languages',
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Key': 'bc97a45c89msh3eda3b2b978fb1fp1f44c1jsn30893d3a6b96',
+//       'X-RapidAPI-Host': 'lecto-translation.p.rapidapi.com'
+//     }
+//   };
   
-  //Use returned data to create user inputs(drop down menu, translate buttons)
-  $.ajax(settings).done(function (response) {
-	  var languageArray = response.languages;
-    var userInputDiv = $(`<div class="user-input gap-4">`);
-    var dropDown = $(`<select name="languages" id="language-select" class="inline mx-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md">`);
-    var defaultDropDown = $(`<option value="" disabled selected>Select a language</option>`);
-    dropDown.append(defaultDropDown);
-    var translateIngredientButton = $(`<button type="submit" id="translate-ingredient-btn" class="mx-1 text-black bg-yellow-500 hover:bg-yellow-600  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Translate Ingredients</button>`);
-    var translateMethodButton = $(`<button type="submit" id="translate-method-btn" class="mx-1 text-black bg-yellow-500 hover:bg-yellow-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Translate Instructions</button>`);
-	  //for loop iterates data object to create each language option with code to be used in translation function
-    for (var i = 0; i < languageArray.length; i++){
-      var languageName = languageArray[i].display_name;
-      var languageCode = languageArray[i].language_code;
-      var languageOption = $(`<option value="${languageCode}">${languageName}</option>`);
-      dropDown.append(languageOption);
-    }
-    userInputDiv.append(dropDown, translateIngredientButton, translateMethodButton);
-    $(".recipe-deck-content").append(userInputDiv);
-  })
-};
+//   //Use returned data to create user inputs(drop down menu, translate buttons)
+//   $.ajax(settings).done(function (response) {
+// 	  var languageArray = response.languages;
+//     var userInputDiv = $(`<div class="user-input gap-4">`);
+//     var dropDown = $(`<select name="languages" id="language-select" class="inline mx-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md">`);
+//     var defaultDropDown = $(`<option value="" disabled selected>Select a language</option>`);
+//     dropDown.append(defaultDropDown);
+//     var translateIngredientButton = $(`<button type="submit" id="translate-ingredient-btn" class="mx-1 text-black bg-yellow-500 hover:bg-yellow-600  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Translate Ingredients</button>`);
+//     var translateMethodButton = $(`<button type="submit" id="translate-method-btn" class="mx-1 text-black bg-yellow-500 hover:bg-yellow-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Translate Instructions</button>`);
+// 	  //for loop iterates data object to create each language option with code to be used in translation function
+//     for (var i = 0; i < languageArray.length; i++){
+//       var languageName = languageArray[i].display_name;
+//       var languageCode = languageArray[i].language_code;
+//       var languageOption = $(`<option value="${languageCode}">${languageName}</option>`);
+//       dropDown.append(languageOption);
+//     }
+//     userInputDiv.append(dropDown, translateIngredientButton, translateMethodButton);
+//     $(".recipe-deck-content").append(userInputDiv);
+//   })
+// };
 
 //function to translate text - parameters gained from listener events for translate buttons
 //API requires parameters of language code and text to translate
@@ -215,7 +240,7 @@ function translate(languageCode, oldText, targetEl){
       //returned translated text and replaces existing text
       targetEl.html(newText);
     })
-    .then((json) => console.log(JSON.stringify(json)))
+    //.then((json) => console.log(JSON.stringify(json)))
     .catch(function (error) {
       console.error(error);
   })
@@ -245,4 +270,5 @@ $(document).ready(function(){
   })
 });
 
+// FLOW-3: fetch supported Languages
 generateLanguageOptionsLecto();
